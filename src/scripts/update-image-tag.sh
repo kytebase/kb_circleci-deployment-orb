fbd70ecc5c42f9229c082e80fdbccbc62f89766e
Mygmarsuren Sedjav <miigaa.sv@gmail.com>
2022-09-28 20:11:45 +0800

    support dev-* branches

--- src/scripts/update-image-tag.sh
+++ src/scripts/update-image-tag.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cd "apps/$PARAM_APP/environments/$CIRCLE_BRANCH/k8s" || exit
+DEPLOY_ENV=$(echo $CIRCLE_BRANCH | grep -oEi '^([a-z]{3,4})')
+
+cd "apps/$PARAM_APP/environments/$DEPLOY_ENV/k8s" || exit
 /home/circleci/project/kustomize edit set image "$PARAM_IMAGE"="$PARAM_IMAGE":"$CIRCLE_SHA1"
