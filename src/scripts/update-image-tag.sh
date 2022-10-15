8dd7abe3b2568851a6850c33acbf4cdcc75087ff
Mygmarsuren Sedjav <miigaa.sv@gmail.com>
2022-10-15 10:48:58 +0800

    support multiple application deploy at once

--- src/scripts/update-image-tag.sh
+++ src/scripts/update-image-tag.sh
@@ -2,5 +2,8 @@
 
 DEPLOY_ENV=$(echo "$CIRCLE_BRANCH" | grep -oEi '^([a-z]{3,4})')
 
-cd "apps/$PARAM_APP/environments/$DEPLOY_ENV/k8s" || exit
-/home/circleci/project/kustomize edit set image "$PARAM_IMAGE"="$PARAM_IMAGE":"$CIRCLE_SHA1"
+for app in $PARAM_APP; do
+    echo "Updating: $app"
+    cd "apps/$app/environments/$DEPLOY_ENV/k8s" || exit
+    /home/circleci/project/kustomize edit set image "$PARAM_IMAGE"="$PARAM_IMAGE":"$CIRCLE_SHA1"
+done
