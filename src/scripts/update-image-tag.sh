4295025bf8b342a6ea5043d3bb0de029702e4696
Mygmarsuren Sedjav <miigaa.sv@gmail.com>
2022-10-15 11:02:25 +0800

    fix

--- src/scripts/update-image-tag.sh
+++ src/scripts/update-image-tag.sh
@@ -4,7 +4,15 @@ DEPLOY_ENV=$(echo "$CIRCLE_BRANCH" | grep -oEi '^([a-z]{3,4})')
 ROOT_DIR=$(pwd)
 
 for app in $PARAM_APP; do
-    echo "Updating: $app"
-    cd "$ROOT_DIR/apps/$app/environments/$DEPLOY_ENV/k8s" || exit
-    /home/circleci/project/kustomize edit set image "$PARAM_IMAGE"="$PARAM_IMAGE":"$CIRCLE_SHA1"
+    K8S_DIR="$ROOT_DIR/apps/$app/environments/$DEPLOY_ENV/k8s"
+
+    if [ ! -d "$K8S_DIR" ] 
+    then
+        echo "Updating: $app"
+        cd $K8S_DIR || exit
+        /home/circleci/project/kustomize edit set image "$PARAM_IMAGE"="$PARAM_IMAGE":"$CIRCLE_SHA1"
+    else
+        echo "Skipped: $app"
+        echo "$K8S_DIR directory does not exist."
+    fi
 done
