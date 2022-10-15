3a6e3233545b592ab7f41a14f833b95f29ef8a49
Mygmarsuren Sedjav <miigaa.sv@gmail.com>
2022-10-15 10:54:51 +0800

    wip

--- src/scripts/update-image-tag.sh
+++ src/scripts/update-image-tag.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DEPLOY_ENV=$(echo "$CIRCLE_BRANCH" | grep -oEi '^([a-z]{3,4})')
+ROOT_DIR=$(pwd)
 
 for app in $PARAM_APP; do
     echo "Updating: $app"
-    cd "apps/$app/environments/$DEPLOY_ENV/k8s" || exit
+    cd "$ROOT_DIR/apps/$app/environments/$DEPLOY_ENV/k8s" || exit
     /home/circleci/project/kustomize edit set image "$PARAM_IMAGE"="$PARAM_IMAGE":"$CIRCLE_SHA1"
 done
